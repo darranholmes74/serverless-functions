b69abfa38580aea5f22131a5b71d167cf298521d
Darran Holmes <darranholmes74@gmail.com>
2023-04-10 19:05:35 -0700

    added GET function

---
+++ serverless_functions/serverless_functions.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        if "word" in dic:
+            url = "https://restcountries.com/v3.1/all"
+            r = requests.get(url + dic["word"])
+            data = r.json()
+            countries = []
+            for word_data in data:
+                country = word_data["name"][0]["common"][0]
+                countries.append[country]
+            message = str(countries)
+
+        else:
+            message = "Give me a country name please"
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
